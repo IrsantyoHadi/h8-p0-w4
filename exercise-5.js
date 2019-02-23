deb12465ee964684d718a3a800dce06faac0bba5
Irsantyo Hadi <irsantyohadi@gmail.com>
2019-02-24 02:45:03 +0700

    perbaikan tugas ubah kata

--- exercise-5.js
+++ exercise-5.js
@@ -5,17 +5,20 @@ function ubahHuruf(kata) {
 
     for (var i=0 ; i<kata.length ; i++){
         for(var j=0 ; j<kamus.length; j++){
-            if(kata[i] === kamus[j]){
+            if(kata[i] === kamus[j] && kata[i] != kamus[kamus.length-1]){
                 kataBaru += kamus[j+1]
             }
+        }
+        if(kata[i] === kamus[kamus.length-1]){
+            kataBaru += kamus[0]
         }
     }
     return kataBaru
   }
   
   // TEST CASES
-  console.log(ubahHuruf('wow')); // xpx
-  console.log(ubahHuruf('developer')); // efwfmpqfs
-  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
-  console.log(ubahHuruf('keren')); // lfsfo
-  console.log(ubahHuruf('semangat')); // tfnbohbu
+  console.log(ubahHuruf('wowz')); // xpx
+//   console.log(ubahHuruf('developer')); // efwfmpqfs
+//   console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+//   console.log(ubahHuruf('keren')); // lfsfo
+//   console.log(ubahHuruf('semangat')); // tfnbohbu
